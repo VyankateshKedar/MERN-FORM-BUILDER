d278a707cc996b8c2073d3ae10ba39c3bd12fddf
Vyankatesh <125432163+VyankateshKedar@users.noreply.github.com>
2025-01-05 00:52:32 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,2 @@
-// config.js
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://mern-form-builder-16.onrender.com"; // Replace with your deployed URL
+const API_BASE_URL = "https://mern-form-builder-16.onrender.com";
 export default API_BASE_URL;
