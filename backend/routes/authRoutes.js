6e7b8a82ce9b3c4d42b54fe4579ef299902feb21
Vyankatesh <125432163+VyankateshKedar@users.noreply.github.com>
2025-01-05 00:43:40 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -53,7 +53,7 @@ router.post('/signup', async (req, res) => {
 });
 
 // Login Route
-router.post('/login', async (req, res) => {
+router.post('/signin', async (req, res) => {
   const { email, password } = req.body;
 
   try {
